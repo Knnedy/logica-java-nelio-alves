ac4fd126118980f3c6fdaaaa77b2f95fee1950c6
kennedy <dkennyvs@gmail.com>
2022-08-07 10:15:15 -0300

    Processamento e casting

---
+++ aula04_ProcessamentoECasting/src/Main.java
@@ -0,0 +1,46 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int x, y;
+		x = 5;
+		y = 2 * x;
+		
+		System.out.println(x);
+		System.out.println(y);
+		
+		int x1;
+		double y1;
+		x1 = 5;
+		y1 = 2 * x;
+		 
+		System.out.println(x1);
+		System.out.println(String.format("%.1f", y1));
+		
+		double b1, b2, h, area;
+		b1 = 6.0;
+		b2 = 8.0;
+		h = 5.0;
+		area = (b1 + b2) / 2.0 * h;
+		
+		System.out.println(area);
+		
+		int a, b, resultado;
+		a = 5;
+		b = 2;
+		resultado = a / b ;
+		
+		System.out.println(resultado);
+		
+		double a1;
+		int c;
+		a1 = 5.0;
+		c = (int) a1;
+		
+		System.out.println(c);
+		
+		
+	}
+
+}
