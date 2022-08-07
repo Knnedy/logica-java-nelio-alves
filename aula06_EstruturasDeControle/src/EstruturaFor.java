9a0406b58c306a5cba81c72ca314d5525def496a
kennedy <dkennyvs@gmail.com>
2022-08-07 13:09:35 -0300

    Estruturuas de controle

--- aula06_EstruturasDeControle/src/EstruturaFor.java
+++ aula06_EstruturasDeControle/src/EstruturaFor.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
 
 public class EstruturaFor {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		int N, i, x, soma;
+		
+		System.out.print("Quantos números serão digitados? ");
+		N = sc.nextInt();
+		
+		soma = 0;
+		for (i = 1; i <= N; i++) {
+			System.out.print("Digite um número: ");
+			x = sc.nextInt();
+			soma = soma + x;
+		}
+		
+		if (soma % 2 == 0) {
+			System.out.println("SOMA = " + soma + " par.");
+		} else {
+			System.out.println("SOMA = " + soma + " ímpar.");
+		}
+		
+		sc.close();
 	}
-
 }
--- aula06_EstruturasDeControle/src/EstruturaWhile.java
+++ aula06_EstruturasDeControle/src/EstruturaWhile.java
@@ -1,9 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class EstruturaWhile {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double C, F;
+		char resposta;
+		
+		do {
+			System.out.print("Digite a temp em Celsius: ");
+			C = sc.nextDouble();
+			F = 9.0 * C / 5.0 + 32.0;
+			System.out.printf("Equivalente em Fahrenheit: %.1f", F , "º");
+			System.out.println("\nDeseja repetir [s/n]?");
+			
+			resposta = sc.next().charAt(0);
+		} while (resposta == 's');
+		sc.close();
+		
 	}
 
 }
