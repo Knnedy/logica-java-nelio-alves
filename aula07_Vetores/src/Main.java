ab7646e7c50e54bfcf739cea46b765c72eb10705
kennedy <dkennyvs@gmail.com>
2022-08-10 11:20:41 -0300

    Vetores

--- aula07_Vetores/src/Main.java
+++ aula07_Vetores/src/Main.java
@@ -7,6 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 
 		Locale.setDefault(Locale.US);
+
 		try (Scanner sc = new Scanner(System.in)) {
 			int N, i;
 			System.out.print("Quantos números você vai digitar? ");
@@ -18,17 +19,16 @@ public class Main {
 				System.out.print("Digite um número: ");
 				doubleVet[i] = sc.nextDouble();
 			}
+
 			System.out.println("\nNÚMEROS DIGITADOS: ");
+			
 			for (i = 0; i < N; i++) {
 				System.out.println(String.format("%.1f", doubleVet[i]));
 			}
 			
 			System.out.println("\nArray gerada: \n" + Arrays.toString(doubleVet));
 			System.out.println("Length do Array: " + doubleVet.length);
-			
-			sc.close();
-		}
-		
-	}
 
+		}
+	}
 }
