9819ed014b8fa58d13ad15a62f2c18aa2ea0e468
kennedy <dkennyvs@gmail.com>
2022-08-10 09:04:42 -0300

    Crescente e Decrescente

---
+++ aula12_ProblemaCrescente/src/Main.java
@@ -0,0 +1,40 @@
+/**
+ * "Problema Crescente"
+ * Leia uma quantidade indeterminada de de dúplas de valores inteiros Xe Y.
+ * Escreva para cada Xe Y uma mensagem que indique se estes valores foram 
+ * digitados em ordem crescente ou decrescente. O programa deve finalizar
+ * quando forem digitados valores iguais.
+ */
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int x, y;
+
+		System.out.println("Digite os dois números: ");
+		x = sc.nextInt();
+		y = sc.nextInt();
+		
+		while (x != y) {
+			if (x < y) {
+				System.out.println("Crescente!");
+			}
+			else {
+				System.out.println("Decrescente!");
+			}
+			System.out.println("Digite outros dois números: ");
+			x = sc.nextInt();
+			y = sc.nextInt();
+	
+		}
+		
+		sc.close();
+	}
+}
