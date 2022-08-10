76631944ac426c882bdc642375f60ebfdcbef6c9
kennedy <dkennyvs@gmail.com>
2022-08-10 10:16:28 -0300

    Vetores

--- aula07_Vetores/src/Main.java
+++ aula07_Vetores/src/Main.java
@@ -7,27 +7,27 @@ public class Main {
 	public static void main(String[] args) {
 
 		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
-		
-		int N, i;
-		System.out.print("Quantos números você vai digitar? ");
-		N = sc.nextInt();
-		
-		double[] doubleVet = new double[N];
-		
-		for (i = 0; i < N; i++) {
-			System.out.print("Digite um número: ");
-			doubleVet[i] = sc.nextDouble();
+		try (Scanner sc = new Scanner(System.in)) {
+			int N, i;
+			System.out.print("Quantos números você vai digitar? ");
+			N = sc.nextInt();
+			
+			double[] doubleVet = new double[N];
+			
+			for (i = 0; i < N; i++) {
+				System.out.print("Digite um número: ");
+				doubleVet[i] = sc.nextDouble();
+			}
+			System.out.println("\nNÚMEROS DIGITADOS: ");
+			for (i = 0; i < N; i++) {
+				System.out.println(String.format("%.1f", doubleVet[i]));
+			}
+			
+			System.out.println("\nArray gerada: \n" + Arrays.toString(doubleVet));
+			System.out.println("Length do Array: " + doubleVet.length);
+			
+			sc.close();
 		}
-		System.out.println("\nNÚMEROS DIGITADOS: ");
-		for (i = 0; i < N; i++) {
-			System.out.println(String.format("%.1f", doubleVet[i]));
-		}
-		
-		System.out.println("\nArray gerada: \n" + Arrays.toString(doubleVet));
-		System.out.println("Tamanho do Array: " + doubleVet.length);
-		
-		sc.close();
 		
 	}
 
