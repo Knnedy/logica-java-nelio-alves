bad9f2fc97ee98876170e789d149cd434a2943ed
kennedy <dkennyvs@gmail.com>
2022-08-10 10:42:52 -0300

    Soma ímpares

--- aula13_ProblemaSomaImpares/src/Main.java
+++ aula13_ProblemaSomaImpares/src/Main.java
@@ -1,4 +1,11 @@
+/**
+ * Problema ""Soma Ímpares
+ * Leia 2 valores inteiros X e Y (em qualuqer ordem). A seguir,
+ * calcule e mostre a soma dos números ímpares entre eles.
+ */
+
 import java.util.Scanner;
+import java.util.Locale;
 
 public class Main {
 
@@ -6,13 +13,28 @@ public class Main {
 
 		try(Scanner sc = new Scanner(System.in)) {
 
+			Locale.setDefault(Locale.US);
 
+			int x, y, troca, soma;
 
+			System.out.println("Digite os dois números: ");
+			x = sc.nextInt();
+			y = sc.nextInt();
 
+			if (x > y) {
+				troca = x;
+				x = y;
+				y = troca;
+			}
 
+			soma = 0;
+			for (int i = x + 1; i < y; i++) {
+				if(i % 2 != 0) {
+					soma = soma + i;
+				}
+			}
 
-
-			
+			System.out.println("SOMA DOS ÍMPARES :" + soma);
 		}
 
 	}
