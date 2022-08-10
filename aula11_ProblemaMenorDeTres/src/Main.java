e1dbd43e65b8cc2a777fae4257661734d88dde76
kennedy <dkennyvs@gmail.com>
2022-08-10 08:34:32 -0300

    Menor de três

---
+++ aula11_ProblemaMenorDeTres/src/Main.java
@@ -0,0 +1,18 @@
+/**
+ * "Problema Menor de três"
+ * Fazer um programa para ler três números inteiros. Em seguida,
+ * mostrar qual o menor dentre os três números lidos. Em caso de empate,
+ * mostrar apenas uma vez.
+ */
+
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
