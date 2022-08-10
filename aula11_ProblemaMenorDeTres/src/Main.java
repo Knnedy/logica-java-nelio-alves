d25b730b91cc9b87d83ff8c0b07c4a8838e52ea3
kennedy <dkennyvs@gmail.com>
2022-08-10 08:42:15 -0300

    Menor de três

--- aula11_ProblemaMenorDeTres/src/Main.java
+++ aula11_ProblemaMenorDeTres/src/Main.java
@@ -5,14 +5,39 @@
  * mostrar apenas uma vez.
  */
 
-
-
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 		
+		int a, b, c, menor;
+
+		System.out.println("Primeiro valor: ");
+		a = sc.nextInt();
+		System.out.println("Segundo valor: ");
+		b = sc.nextInt();
+		System.out.println("Terceiro valor: ");
+		c = sc.nextInt();
+		
+		if (a < b && a < c) {
+			menor = a;
+		} 
+		else if (b < c && b < a) {
+			menor = b;
+		}
+		else {
+			menor = c;
+		}
+		
+		System.out.println("MENOR = " + menor);
+		
+		
+		sc.close();
 	}
 
 }
