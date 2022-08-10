011f571442ebe9f324ed914e8852ae1440d95d14
kennedy <dkennyvs@gmail.com>
2022-08-10 10:15:58 -0300

    Estrutura de controle

--- aula06_EstruturasDeControle/src/EstruturaFor.java
+++ aula06_EstruturasDeControle/src/EstruturaFor.java
@@ -4,25 +4,26 @@ public class EstruturaFor {
 
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
-		int N, i, x, soma;
-		
-		System.out.print("Quantos números serão digitados? ");
-		N = sc.nextInt();
-		
-		soma = 0;
-		for (i = 1; i <= N; i++) {
-			System.out.print("Digite um número: ");
-			x = sc.nextInt();
-			soma = soma + x;
+		try (Scanner sc = new Scanner(System.in)) {
+			int N, i, x, soma;
+			
+			System.out.print("Quantos números serão digitados? ");
+			N = sc.nextInt();
+			
+			soma = 0;
+			for (i = 1; i <= N; i++) {
+				System.out.print("Digite um número: ");
+				x = sc.nextInt();
+				soma = soma + x;
+			}
+			
+			if (soma % 2 == 0) {
+				System.out.println("SOMA = " + soma + " par.");
+			} else {
+				System.out.println("SOMA = " + soma + " ímpar.");
+			}
+			
+			sc.close();
 		}
-		
-		if (soma % 2 == 0) {
-			System.out.println("SOMA = " + soma + " par.");
-		} else {
-			System.out.println("SOMA = " + soma + " ímpar.");
-		}
-		
-		sc.close();
 	}
 }
