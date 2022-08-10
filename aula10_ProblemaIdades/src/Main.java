93af2db41e59351b3fba190bcf18e79235cf87bf
kennedy <dkennyvs@gmail.com>
2022-08-10 08:34:05 -0300

    Problema Idades

---
+++ aula10_ProblemaIdades/src/Main.java
@@ -0,0 +1,45 @@
+/**
+ * Problema "idades"
+ * Fazer um programa para ler o nome e idade de duas pessoas.
+ * Ao final mostrar uma mensagem com os nomes e a idade média
+ * entre essas pessoas, com uma casa decimal, conforme exemplo.
+ */
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		String nome1, nome2;
+		int idade1, idade2;
+		double media;
+		
+		System.out.println("Dados da da primeira pessoa: ");
+		System.out.print("Nome: ");
+		nome1 = sc.nextLine();
+		System.out.print("Idade: ");
+		idade1 = sc.nextInt();
+		
+		System.out.println("Dados da da segunda pessoa: ");
+		System.out.print("Nome: ");
+		sc.nextLine();
+		nome2 = sc.nextLine();
+		System.out.print("Idade: ");
+		idade2 = sc.nextInt();
+		
+		media = (double)(idade1 + idade2) / 2.0;
+		System.out.println("A idade media de " + nome1 + " e " + nome2 + " é de "
+				+ String.format("%.1f", media) + " anos.");
+		
+		
+		
+		
+		sc.close();
+	}
+
+}
